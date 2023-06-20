1e6cc1fee1a44f3599b915b112a6fb390cee66c7
ali <codewithkiani@gmail.com>
2023-06-20 08:04:44 +0330

    Adding first example route

--- main.go
+++ main.go
@@ -4,5 +4,10 @@ import "github.com/gofiber/fiber/v2"
 
 func main() {
 	app := fiber.New()
+	app.Get("/", handleHome)
 	app.Listen(":5000")
+}
+
+func handleHome(c *fiber.Ctx) error {
+	return c.JSON(map[string]string{"msg": "Its Working"})
 }
