1f34571d96e87996662523233b0ca1f2c0bd177f
ali <codewithkiani@gmail.com>
2023-06-21 08:19:09 +0330

    Create a flag for api server port

--- main.go
+++ main.go
@@ -1,11 +1,19 @@
 package main
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"flag"
+
+	"github.com/gofiber/fiber/v2"
+)
 
 func main() {
+	listenPort := flag.String("port", ":5000", "The port address for api server")
+	flag.Parse()
+
 	app := fiber.New()
 	app.Get("/", handleHome)
-	app.Listen(":5000")
+
+	app.Listen(*listenPort)
 }
 
 func handleHome(c *fiber.Ctx) error {
