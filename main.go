2d5896cae291d23aee96d4e2ac55132835f69a7d
ali <codewithkiani@gmail.com>
2023-06-27 17:04:11 +0330

    adding some constant

--- main.go
+++ main.go
@@ -3,14 +3,18 @@ package main
 import (
 	"context"
 	"flag"
+	"fmt"
 	"log"
 
+	"github.com/ItsKiani/mahfel/types"
 	"github.com/gofiber/fiber/v2"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
 const dbUri = "mongodb://localhost:27017"
+const dbName = "froum"
+const userColl = "users"
 
 func main() {
 	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dbUri))
