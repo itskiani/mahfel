1c84671aba2663ea6e6a02178b513f19fe1c9917
ali <codewithkiani@gmail.com>
2023-06-27 16:56:03 +0330

    Configure mongodb

--- main.go
+++ main.go
@@ -1,12 +1,23 @@
 package main
 
 import (
+	"context"
 	"flag"
+	"log"
 
 	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+const dbUri = "mongodb://localhost:27017"
+
 func main() {
+	client, err := mongo.Connect(context.TODO(), options.Client().ApplyURI(dbUri))
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	listenPort := flag.String("port", ":5000", "The port address for api server")
 	flag.Parse()
 
