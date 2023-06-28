6985b9cebf5b29ea5b227d90a691b2b58ccdf89c
ali <codewithkiani@gmail.com>
2023-06-28 16:06:19 +0330

    Modify Id field in User Type

--- types/user.go
+++ types/user.go
@@ -1,11 +1,15 @@
 package types
 
-import "time"
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
 
 type User struct {
-	Id        string    `bson:"_id,omitempty" json:"id,omitempty"`
-	FirstName string    `bson:"first_name" json:"first_name"`
-	LastName  string    `bson:"last_name" json:"last_name"`
-	CreatedAt time.Time `bson:"created_at" json:"created_at"`
-	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
+	Id        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	FirstName string             `bson:"first_name" json:"first_name"`
+	LastName  string             `bson:"last_name" json:"last_name"`
+	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
 }
