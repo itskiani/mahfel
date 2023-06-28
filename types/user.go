dd34bb6d1386909360f10b637e037352f531d0bf
ali <codewithkiani@gmail.com>
2023-06-28 15:45:45 +0330

    Modify type User

--- types/user.go
+++ types/user.go
@@ -2,6 +2,6 @@ package types
 
 type User struct {
 	Id        string `bson:"_id,omitempty" json:"id,omitempty"`
-	FirstName string `bson:"firstName" json:"firstName"`
-	LastName  string `bson:"lastName" json:"lastName"`
+	FirstName string `bson:"first_name" json:"first_name"`
+	LastName  string `bson:"last_name" json:"last_name"`
 }
