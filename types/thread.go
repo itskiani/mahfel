7a784956e00dce740dc969b0703f33c73eff19f8
ali <codewithkiani@gmail.com>
2023-06-28 16:13:07 +0330

    Modify Thread type

--- types/thread.go
+++ types/thread.go
@@ -1,1 +1,16 @@
 package types
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Thread struct {
+	Id        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	UserId    int                `bson:"user_id" json:"user_id"`
+	Title     string             `bson:"title" json:"title"`
+	Body      string             `bson:"body" json:"body"`
+	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
+}
