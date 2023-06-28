3078039fd7c4aac2d9347535cef4321d60ca0c61
ali <codewithkiani@gmail.com>
2023-06-28 15:56:39 +0330

    Adding two field in User type

--- types/user.go
+++ types/user.go
@@ -1,7 +1,11 @@
 package types
 
+import "time"
+
 type User struct {
-	Id        string `bson:"_id,omitempty" json:"id,omitempty"`
-	FirstName string `bson:"first_name" json:"first_name"`
-	LastName  string `bson:"last_name" json:"last_name"`
+	Id        string    `bson:"_id,omitempty" json:"id,omitempty"`
+	FirstName string    `bson:"first_name" json:"first_name"`
+	LastName  string    `bson:"last_name" json:"last_name"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at" json:"updated_at"`
 }
