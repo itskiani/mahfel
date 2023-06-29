4c120a973bb48759bd0114391f901d1f690a7f6a
ali <codewithkiani@gmail.com>
2023-06-29 06:02:24 +0330

    Modify Reply type

--- types/reply.go
+++ types/reply.go
@@ -1,1 +1,16 @@
 package types
+
+import (
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+type Reply struct {
+	Id        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	UserId    int                `bson:"user_id" json:"user_id"`
+	ThreadId  int                `bson:"thread_id" json:"thread_id"`
+	Body      string             `bson:"body" json:"body"`
+	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
+}
