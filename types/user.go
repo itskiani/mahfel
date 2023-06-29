c79a2c940afb14fff68f81c3cbe1f6c79b878db8
ali <codewithkiani@gmail.com>
2023-06-29 08:48:35 +0330

    Adding a field in User type

--- types/user.go
+++ types/user.go
@@ -8,6 +8,7 @@ import (
 
 type User struct {
 	Id        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Email     string             `bson:"email" json:"email"`
 	FirstName string             `bson:"first_name" json:"first_name"`
 	LastName  string             `bson:"last_name" json:"last_name"`
 	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
