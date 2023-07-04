cc7d09334a191ae59f3d8fdf8ba6c9a8e13fe812
ali <codewithkiani@gmail.com>
2023-07-04 17:30:03 +0330

    Adding validation for user type

--- types/user.go
+++ types/user.go
@@ -8,10 +8,10 @@ import (
 
 type User struct {
 	Id        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	Email     string             `bson:"email" json:"email"`
-	Password  string             `bson:"password" json:"_"`
-	FirstName string             `bson:"first_name" json:"first_name"`
-	LastName  string             `bson:"last_name" json:"last_name"`
+	Email     string             `bson:"email" json:"email" validate:"email, required"`
+	Password  string             `bson:"password" json:"_" validate:"required, min=6"`
+	FirstName string             `bson:"first_name" json:"first_name" validate:"required, min=2, max=100"`
+	LastName  string             `bson:"last_name" json:"last_name" validate:"required, min=2, max=100"`
 	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
 	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
 }
