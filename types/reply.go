d345394d5a1cef6b1cf3a59f563a3cc8a4ce11a7
ali <codewithkiani@gmail.com>
2023-07-04 17:33:20 +0330

    Adding validation for reply type

--- types/reply.go
+++ types/reply.go
@@ -8,9 +8,9 @@ import (
 
 type Reply struct {
 	Id        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	UserId    int                `bson:"user_id" json:"user_id"`
-	ThreadId  int                `bson:"thread_id" json:"thread_id"`
-	Body      string             `bson:"body" json:"body"`
+	UserId    int                `bson:"user_id" json:"user_id" validate:"required"`
+	ThreadId  int                `bson:"thread_id" json:"thread_id" validate:"required"`
+	Body      string             `bson:"body" json:"body" validate:"required"`
 	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
 	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
 }
