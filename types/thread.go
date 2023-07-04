59afd46aa79253ca4dd3eb022086ac63c96775cd
ali <codewithkiani@gmail.com>
2023-07-04 17:34:58 +0330

    Adding validation for thread type

--- types/thread.go
+++ types/thread.go
@@ -8,9 +8,9 @@ import (
 
 type Thread struct {
 	Id        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
-	UserId    int                `bson:"user_id" json:"user_id"`
-	Title     string             `bson:"title" json:"title"`
-	Body      string             `bson:"body" json:"body"`
+	UserId    int                `bson:"user_id" json:"user_id" validate:"required"`
+	Title     string             `bson:"title" json:"title" validate:"required"`
+	Body      string             `bson:"body" json:"body" validate:"required"`
 	CreatedAt time.Time          `bson:"created_at" json:"created_at"`
 	UpdatedAt time.Time          `bson:"updated_at" json:"updated_at"`
 }
