adb4e2541bf5f272574607b7022097ba8f965f49
ali <codewithkiani@gmail.com>
2023-07-13 23:23:36 +0330

    Add UserHandler type in api module

--- api/routes/user_handlers.go
+++ api/routes/user_handlers.go
@@ -1,6 +1,13 @@
 package api
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/ItsKiani/mahfel/db"
+	"github.com/gofiber/fiber/v2"
+)
+
+type UserHandler struct {
+	userStorage db.MongoUserStorage
+}
 
 func HandleGetUser(c *fiber.Ctx) error {
 	id := c.Params("id")
