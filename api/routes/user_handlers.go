f0fce52f4aa374c7e614ec2b5fe87fd0f193c6b9
ali <codewithkiani@gmail.com>
2023-07-13 23:17:45 +0330

    Add HandleGetUser function

--- api/routes/user_handlers.go
+++ api/routes/user_handlers.go
@@ -2,6 +2,11 @@ package api
 
 import "github.com/gofiber/fiber/v2"
 
+func HandleGetUser(c *fiber.Ctx) error {
+	id := c.Params("id")
+	return c.JSON(id)
+}
+
 func HandleGetUsers(c *fiber.Ctx) error {
 	return c.JSON("Kiani")
 }
