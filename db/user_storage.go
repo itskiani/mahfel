d5d9b5beb9df08a1626c48d860f816a609ddac08
ali <codewithkiani@gmail.com>
2023-07-14 16:31:05 +0330

    Add NewMongoUserStorage function in user_storage.go

--- db/user_storage.go
+++ db/user_storage.go
@@ -1,9 +1,18 @@
 package db
 
-import types "github.com/ItsKiani/mahfel/types"
+import (
+	types "github.com/ItsKiani/mahfel/types"
+	"go.mongodb.org/mongo-driver/mongo"
+)
 
 type UserStorage interface {
 	GetUserByID(string) (*types.User, error)
 }
 
 type MongoUserStorage struct{}
+
+func NewMongoUserStorage(c *mongo.Client) *MongoUserStorage {
+	return &MongoUserStorage{
+		client: client,
+	}
+}
