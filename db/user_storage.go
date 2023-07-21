f4ceb62ce0c7629d6ccc6fba694ef0e0b490bace
ali <codewithkiani@gmail.com>
2023-07-21 10:20:49 +0330

    modify type MongoUserStorage and update NewMongoUserStorage function

--- db/user_storage.go
+++ db/user_storage.go
@@ -1,22 +1,26 @@
 package db
 
 import (
+	"os"
+
 	types "github.com/ItsKiani/mahfel/types"
 	"go.mongodb.org/mongo-driver/mongo"
 )
+
+const userColl = "users"
 
 type UserStorage interface {
 	GetUserByID(string) (*types.User, error)
 }
 
 type MongoUserStorage struct {
-	client *mongo.Client
-	dbname string
-	coll   *mongo.Collection
+	client     *mongo.Client
+	collection *mongo.Collection
 }
 
 func NewMongoUserStorage(client *mongo.Client) *MongoUserStorage {
 	return &MongoUserStorage{
-		client: client,
+		client:     client,
+		collection: client.Database(os.Getenv("DB_NAME")).Collection(userColl),
 	}
 }
